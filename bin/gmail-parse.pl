2b45203a3067af766c7ad6f8fee9e65239bbbba7
Gavin Miller <gavingmiller@gmail.com>
2012-07-16 10:05:28 -0600

    Moved parsing into a seperate perl script.

---
+++ bin/gmail-parse.pl
@@ -0,0 +1,34 @@
+$green = "\033[01;32m";
+$yellow = "\033[01;33m";
+$blue = "\033[01;34m";
+$normal = "\033[01;37m";
+
+while (defined($line = <STDIN>)) {
+  chomp($line);
+
+  if ($line =~ /<title>/ and $line =~ !/Gmail - Inbox/) {
+    print "\n"
+  }
+
+  if ($line =~ /<(title)>(.*)<\/\1>/ and $line !~ /Gmail - Inbox/) {
+    print "$green$2$normal\n"
+  }
+
+
+  if ($line =~ /<name>/) {
+    print "    "
+  }
+
+  if ($line =~ /<(name)>(.*)<\/\1>/) {
+    print "$blue$2$normal\n"
+  }
+
+
+  if ($line =~ /<summary>/) {
+    print "  "
+  }
+
+  if ($line =~ /<(summary)>(.*)<\/\1>/ and ($2 ne "")) {
+    print "$yellow$2$normal\n"
+  }
+}
