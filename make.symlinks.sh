f3357eb6b6d0249e9480ef329c0ee791743cfbc9
Gavin Miller <gavmille@cisco.com>
2015-08-05 12:04:59 -0600

    Created script for moving dotfiles and linking appropriately

---
+++ make.symlinks.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+############################################################################################
+# make.symlinks.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# Cribbed from: https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
+
+########## Variables
+dir=~/dotfiles                    # dotfiles directory
+bakdir=~/dotfiles.bak             # dotfiles backup directory
+
+# files/folders to symlink in homedir
+links="bin \
+      .ackrc \
+      .bash \
+      .bashrc \
+      .bash_profile \
+      .gitconfig \
+      .githelpers \
+      .pryrc \
+      .vim \
+      .vimrc"
+
+########## Main
+echo -n "Creating $bakdir for backup of any existing dotfiles in ~ ... "
+mkdir -p $bakdir
+echo "done"
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+
+# move any existing dotfiles in homedir to dotfiles.bak directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $links
+for link in $links; do
+    echo "Moving any existing dotfiles from ~ to $bakdir"
+    mv ~/.$link ~/$bakdir/
+    echo "Creating symlink to $link in home directory."
+    ln -s $dir/$link ~/.$link
+done
